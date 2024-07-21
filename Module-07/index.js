832ca85329cbd8f693be58b128c4f56aaa137ae4
zyphyxx <mendesbr92@gmail.com>
2024-07-20 23:32:08 -0300

    pequeno proj. de um portifolio usando js.

--- Module-07/index.js
+++ Module-07/index.js
@@ -1,17 +1,52 @@
+let h1 = document.querySelector('.titulo').innerHTML = `Dev. Edgar`;
 
-let h1 = document.querySelector('h1').innerHTML = 'Algumas ideias de projetos'; // titulo
+let aboutMe = `Eu sou <strong>Edgar Sousa Mendes</strong>, um entusiasta de tecnologia e apaixonado por <strong>Desenvolvimento web</strong>.
+             Atualmente, estou explorando a área de <strong>Engenharia de Software</strong>.
+              Resido em Suzano, São Paulo, com minha família, que é minha principal fonte de inspiração.`;
 
-let div = document.querySelector('.container-1');
-let ul = document.createElement('ul');
-div.append(ul);
+let jorney = `Iniciei minha jornada na programação Java em 2023 e desde então tenho me dedicado ao aprimoramento constante.
+             No momento, estou focado no desenvolvimento ágil e eficiente utilizando <strong>Spring Boot</strong>.
+              Além disso, possuo conhecimentos básicos em <strong>JavaScript, HTML, CSS, SQL e Node.js com Express</strong>.`
 
-div.before('antes da div');
-div.after('depois da div');
+let arr = ['Desenvolvimento Web: HTML, CSS, JavaScript, Node.js, Express',
+  'Java: Spring Boot',
+  'Banco de Dados: SQL (MySQL)',
+  'Ferramentas: Git, Swagger',
+  'Design Responsivo: Adaptabilidade para diferentes dispositivos'];
 
-for(let i = 0; i < 5; i++){
-  let li = document.createElement('li');
-  li.innerHTML = `item da lista: ${i + 1}`
-  ul.append(li);
+let about = art('.about-me', 'Sobre Mim', aboutMe);
+art('.jorney', 'Jornada Profissional', jorney);
+art2('.skills', 'Habilidades', arr);
+
+
+function art(className, textAbout, textContent) {
+  let article = document.querySelector(className);
+
+  let h2 = document.createElement('h2');
+  h2.innerHTML = textAbout;
+  article.append(h2);
+
+  let p = document.createElement('p');
+  p.innerHTML = textContent
+  article.append(p);
 }
+
+function art2(className, textAbout, arrayItens) {
+  let article = document.querySelector(className);
+  let h2 = document.createElement('h2');
+  h2.innerHTML = textAbout;
+
+
+  let ul = document.querySelector('.list');
+
+  arrayItens.forEach(itens => {
+    let li = document.createElement('li');
+    li.textContent = itens;
+    ul.append(li)
+  });
+
+  article.append(h2, ul);
+}
+
 
 
